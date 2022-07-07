a43e0fc08471f75b39ade6e296a87d734210a5bb
navjot <navjotsandhu910@outlook.com>
2022-07-08 01:52:07 +0530

    Hoop_Jump

---
+++ Hoop_Jump.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t = 0, n = 0;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        cout << (n + 1) / 2 << '\n';
+    }
+    return 0;
+}
+//here i learn every time loops not necessary find some pattren and think out of the box
+//i written this code using one nested loop and tones of if condition but solution is so simple
