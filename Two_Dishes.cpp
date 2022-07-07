94fe1b400fd54b88194cdd181db3e1cd35e110ff
navjot <navjotsandhu910@outlook.com>
2022-07-08 02:11:55 +0530

    Two_Dishes

---
+++ Two_Dishes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, a, b, c;
+        cin>>n>>a>>b>>c;
+        if (a + c >= n && b >= n)
+        {
+            cout << "YES" << '\n';
+        }
+        else
+        {
+            cout << "NO" << '\n';
+        }
+    }
+    return 0;
+}
